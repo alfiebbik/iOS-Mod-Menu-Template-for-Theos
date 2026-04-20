f4c70b20ad87f47d6338ba3152d8b00c739d4a9e
alfiebbik <sonicspeedyboy101@gmail.com>
2026-04-20 03:01:42 +0100

    Refactor Menu setupMenu method
    Refactored setupMenu method to simplify UI customization and updated button descriptions.

--- template/Menu.mm
+++ template/Menu.mm
@@ -1,37 +1,18 @@
 #import <Foundation/Foundation.h>
 #import "Menu.h"
 
-@interface Menu ()
-@property (assign, nonatomic) CGPoint lastMenuLocation;
-@property (strong, nonatomic) UILabel *menuTitle;
-@property (strong, nonatomic) UIView *header;
-@property (strong, nonatomic) UIView *footer;
-@end
-
 @implementation Menu
 
-// This is where you customize your UI look
 - (void)setupMenu {
-    self.menuTitle.text = @"ALFIE CLIENT V1"; // Your Custom Title
-    self.menuTitle.textColor = [UIColor cyanColor];
+    // This sets your title
+    [self setTitle:@"ALFIE CLIENT V1"];
     
-    // Add your custom features here
-    [self addSwitch:@"Stash Finder" description:@"Finds Chests, Hoppers & Spawners"];
-    [self addSwitch:@"Freecam" description:@"Move camera outside your body"];
-    [self addSwitch:@"Fullbright" description:@"See clearly in the dark"];
-    [self addSwitch:@"Speed Hack" description:@"Move faster than normal"];
+    // This adds your custom buttons
+    [self addSwitch:@"Stash Finder" description:@"Finds Chests & Spawners"];
+    [self addSwitch:@"Freecam" description:@"Fly through walls"];
+    [self addSwitch:@"Fullbright" description:@"See in the dark"];
     
-    // Credits
-    [self addLabel:@"Created by Alfie"];
-}
-
-// These are the actual buttons for the menu
-- (void)addSwitch:(NSString *)name description:(NSString *)description {
-    // This is a template function that the menu uses to create buttons
-}
-
-- (void)addLabel:(NSString *)text {
-    // This adds text to your menu
+    [self addLabel:@"Made by Alfie"];
 }
 
 @end
