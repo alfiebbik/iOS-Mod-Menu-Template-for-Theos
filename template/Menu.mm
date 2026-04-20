a2a24074fdcaf6f4010f5a9a5a2df05a0469327e
alfiebbik <sonicspeedyboy101@gmail.com>
2026-04-20 03:06:22 +0100

    Simplify Menu implementation by removing unused methods
    Removed unnecessary switch and label methods from Menu implementation.

--- template/Menu.mm
+++ template/Menu.mm
@@ -4,19 +4,8 @@
 @implementation Menu
 
 - (void)setupMenu {
-    // This is the title at the top of your menu
+    // This is the ONLY line we are changing
     self.menuTitle.text = @"ALFIE CLIENT V1";
-    
-    // These add your switches
-    [self addSwitch:@"Stash Finder" description:@"Finds Chests"];
-    [self addSwitch:@"Freecam" description:@"Fly Mode"];
-    
-    // Your Credits
-    [self addLabel:@"By Alfie"];
 }
-
-// These are helper functions the template needs
-- (void)addSwitch:(NSString *)name description:(NSString *)description {}
-- (void)addLabel:(NSString *)text {}
 
 @end
